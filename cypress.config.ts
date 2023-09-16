87b586f64034d6b8ee064bbf932c8914c3dc7760
Dean Spencer <dean_spencer@yahoo.co.uk>
2023-09-16 21:23:47 +0100

    remove unwanted comments

--- cypress.config.ts
+++ cypress.config.ts
@@ -27,36 +27,3 @@ export default defineConfig({
     },
   },
 });
-
-// import { defineConfig } from "cypress";
-// import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
-// import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-// // @ts-ignore
-// import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
-
-// async function setupNodeEvents(
-//   on: Cypress.PluginEvents,
-//   config: Cypress.PluginConfigOptions
-// ): Promise<Cypress.PluginConfigOptions> {
-//   await addCucumberPreprocessorPlugin(on, config);
-
-//   on(
-//     "file:preprocessor",
-//     createBundler({
-//       plugins: [createEsbuildPlugin(config)],
-//     })
-//   );
-
-//   return config;
-// }
-
-// export default defineConfig({
-//   env: {
-//     url: "your base url",
-//   },
-//   e2e: {
-//     baseUrl: "https://limebread.co.uk",
-//     specPattern: "cypress/features/**/*.feature",
-//     setupNodeEvents,
-//   },
-// });
