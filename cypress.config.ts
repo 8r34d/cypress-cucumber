75a9812ac5f6288ecaf79238c831c150339e25e1
Dean Spencer <dean_spencer@yahoo.co.uk>
2023-11-25 11:10:40 +0000

    add esbuild-ts duckduckgo example

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,29 +1,28 @@
 import { defineConfig } from "cypress";
 import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
-// @ts-ignore
-import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
+
+async function setupNodeEvents(
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions
+): Promise<Cypress.PluginConfigOptions> {
+  await addCucumberPreprocessorPlugin(on, config);
+
+  on(
+    "file:preprocessor",
+    createBundler({
+      plugins: [createEsbuildPlugin(config)],
+    })
+  );
+
+  return config;
+}
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://limebread.co.uk",
+    baseUrl: "https://duckduckgo.com",
     specPattern: "**/*.feature",
-    async setupNodeEvents(
-      on: Cypress.PluginEvents,
-      config: Cypress.PluginConfigOptions
-    ): Promise<Cypress.PluginConfigOptions> {
-      // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
-      await addCucumberPreprocessorPlugin(on, config);
-
-      on(
-        "file:preprocessor",
-        createBundler({
-          plugins: [createEsbuildPlugin(config)],
-        })
-      );
-
-      // Make sure to return the config object as it might have been modified by the plugin.
-      return config;
-    },
+    setupNodeEvents,
   },
 });
--- cypress/e2e/sample/sample.feature
+++
@@ -1,8 +0,0 @@
-Feature: Sample Feature
-
-  Scenario: Sample Scenario
-
-    Given I visit the home page
-    When I view the home page
-    Then I should see the header "Welcome to limebread"
-    And I should see the paragraph "This site is under construction"
