e0eb3f59b6aa4b23209e79eb5c261ddee70494f1
Dean Spencer <dean_spencer@yahoo.co.uk>
2023-11-25 14:00:21 +0000

    parse config file using config.env.configFile

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,28 @@ import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
 import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
 import { createEsbuildPlugin } from "@badeball/cypress-cucumber-preprocessor/esbuild";
 
+import fs from "fs-extra";
+import path from "path";
+
+function getConfigurationByFile(file: string) {
+  const pathToConfigFile = path.resolve("cypress", "config", `${file}.json`);
+
+  return fs.readJson(pathToConfigFile);
+}
+
 async function setupNodeEvents(
   on: Cypress.PluginEvents,
   config: Cypress.PluginConfigOptions
 ): Promise<Cypress.PluginConfigOptions> {
+  // --env configFile=<filename>
+  // filename : duck.demo | azure.lime | gcp.lime | host.env
+  const configFile = config.env.configFile || "duck.demo";
+  const settings = await getConfigurationByFile(configFile);
+  config = {
+    ...config,
+    ...settings,
+  };
+
   await addCucumberPreprocessorPlugin(on, config);
 
   on(
@@ -21,7 +39,6 @@ async function setupNodeEvents(
 
 export default defineConfig({
   e2e: {
-    baseUrl: "https://duckduckgo.com",
     specPattern: "**/*.feature",
     setupNodeEvents,
   },
