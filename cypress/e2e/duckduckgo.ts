9e1d90770e623d2d6c472efcd43cc841be06e816
Dean Spencer <dean_spencer@yahoo.co.uk>
2023-11-26 10:58:17 +0000

    remove ci default

--- cypress/e2e/duckduckgo.ts
+++ cypress/e2e/duckduckgo.ts
@@ -16,6 +16,8 @@ Then("I should see a search bar", () => {
 
   cy.dataCy("greeting").should("not.exist");
 
+  Cypress.env("greeting").should.be(Cypress.env("configFile"));
+
   cy.log("greeting", Cypress.env("greeting"));
   cy.log("host", Cypress.env("host"));
   cy.log("environment", Cypress.env("environment"));
