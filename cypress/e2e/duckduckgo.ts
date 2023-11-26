1672eeee7e58aa76b9dde70adbd4b3c95e104f65
Dean Spencer <dean_spencer@yahoo.co.uk>
2023-11-26 11:17:38 +0000

    update to simpler step

--- cypress/e2e/duckduckgo.ts
+++ cypress/e2e/duckduckgo.ts
@@ -1,18 +1,18 @@
 import { When, Then } from "@badeball/cypress-cucumber-preprocessor";
 
 When("I visit duckduckgo.com", () => {
-  cy.visit("/");
+  cy.visit("https://limebread.co.uk");
 });
 
 Then("I should see a search bar", () => {
-  cy.get("input[type=text]")
-    .should("have.attr", "placeholder")
-    .and(
-      "match",
-      /Search the web without being tracked|Search without being tracked/
-    );
+  // cy.get("input[type=text]")
+  //   .should("have.attr", "placeholder")
+  //   .and(
+  //     "match",
+  //     /Search the web without being tracked|Search without being tracked/
+  //   );
 
-  assert.deepEqual({}, {});
+  // assert.deepEqual({}, {});
 
   cy.dataCy("greeting").should("not.exist");
 
